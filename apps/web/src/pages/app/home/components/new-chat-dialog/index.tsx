ac2153708ccd072d821ba94b4004c526ccb3678c
Bellorico323 <mabo.orico@gmail.com>
2025-03-31 21:46:37 -0300

    new: new chat dialog

---
+++ apps/web/src/pages/app/home/components/new-chat-dialog/index.tsx
@@ -0,0 +1,123 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { RecentChats } from './recent-chats'
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useDebounce } from '@/hooks/use-debounce'
+import { useQuery } from '@tanstack/react-query'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import { useEffect } from 'react'
+
+interface NewChatDialogProps {
+  open: boolean
+  setOpen: (value: boolean) => void
+}
+
+const searchFormSchema = z.object({
+  search: z.string().min(1, { message: 'Minimun of 1 character' }),
+})
+
+type SearchFormSchema = z.infer<typeof searchFormSchema>
+
+export function NewChatDialog({ open, setOpen }: NewChatDialogProps) {
+  const form = useForm<SearchFormSchema>({
+    resolver: zodResolver(searchFormSchema),
+    defaultValues: {
+      search: '',
+    },
+  })
+
+  const { watch, reset } = form
+
+  const searchQuery = watch('search')
+  const debouncedSearch = useDebounce(searchQuery)
+
+  const {
+    data: chats,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['chats', debouncedSearch],
+    queryFn: () => {
+      console.log(debouncedSearch) // TODO: implement query
+      return []
+    },
+    enabled: !!debouncedSearch,
+    staleTime: 5 * 60 * 1000, // 5 minutes
+  })
+
+  useEffect(() => {
+    return () => reset()
+  }, [open, reset])
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>New Chat</DialogTitle>
+          <DialogDescription>Create new chat</DialogDescription>
+        </DialogHeader>
+
+        <div className="flex flex-col gap-6">
+          <Form {...form}>
+            <form>
+              <FormField
+                control={form.control}
+                name="search"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="Search"
+                        {...field}
+                        autoComplete="off"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+
+          <div className="flex flex-col gap-2.5">
+            <p>Recent chats</p>
+            <RecentChats />
+          </div>
+
+          <ScrollArea className="max-h-[14em]">
+            <div className="flex flex-col gap-2.5 pr-3">
+              <p>A</p>
+
+              <div className="flex flex-col  bg-message rounded">
+                {Array.from({ length: 10 }).map((item, idx) => (
+                  <div
+                    className="border-b border-accent py-1.5 flex gap-2.5 items-center last:border-none hover:bg-input/30 px-3"
+                    key={idx}
+                  >
+                    <Avatar className="size-8">
+                      <AvatarImage
+                        src={'https://github.com/Bellorico323.png'}
+                      />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+
+                    <p>Ana Julia</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </ScrollArea>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- apps/web/src/pages/app/home/components/sidebar/sidebar-header.tsx
+++ apps/web/src/pages/app/home/components/sidebar/sidebar-header.tsx
@@ -8,13 +8,20 @@ import {
 import { MessageCirclePlus, Plus, UserPlus } from 'lucide-react'
 import { AddNewContactDialog } from '../add-contact-dialog'
 import { useState } from 'react'
+import { NewChatDialog } from '../new-chat-dialog'
 
 export function SidebarHeader() {
   const [isAddNewContactDialogOpen, setIsAddNewContactDialogOpen] =
     useState(false)
 
+  const [isNewChatDialogOpen, setIsNewChatDialogOpen] = useState(false)
+
   function handleAddNewContactDialog(value: boolean) {
     setIsAddNewContactDialogOpen(value)
+  }
+
+  function handleNewChatDialog(value: boolean) {
+    setIsNewChatDialogOpen(value)
   }
 
   return (
@@ -30,13 +37,15 @@ export function SidebarHeader() {
         <DropdownMenuContent side="bottom" align="end">
           <DropdownMenuItem
             className="p-3"
-            textValue="test"
             onSelect={() => handleAddNewContactDialog(true)}
           >
             <UserPlus />
             Add contact
           </DropdownMenuItem>
-          <DropdownMenuItem className="p-3">
+          <DropdownMenuItem
+            className="p-3"
+            onSelect={() => handleNewChatDialog(true)}
+          >
             <MessageCirclePlus />
             New chat
           </DropdownMenuItem>
@@ -47,6 +56,8 @@ export function SidebarHeader() {
         open={isAddNewContactDialogOpen}
         setOpen={handleAddNewContactDialog}
       />
+
+      <NewChatDialog open={isNewChatDialogOpen} setOpen={handleNewChatDialog} />
     </div>
   )
 }
